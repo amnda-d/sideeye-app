c4ecb9bfb11bd604a09d8f8ae1629bdf7caaf752
Amanda Doucette <amanda@amnda.me>
2020-03-30 11:35:39 -0400

    eyes

--- src/renderer/title.tsx
+++ src/renderer/title.tsx
@@ -4,7 +4,12 @@ import { colors } from "renderer/colors";
 
 export class Title extends React.Component {
   render() {
-    return <Wrapper>SideEye</Wrapper>;
+    return (
+      <Wrapper>
+        <div>SideEye</div>
+        <div class="eye"></div>
+      </Wrapper>
+    );
   }
 }
 
@@ -16,4 +21,53 @@ const Wrapper = styled.div`
   font-size: 20px;
   color: ${colors.text};
   font-weight: bold;
+
+  .eye {
+    width: 20px;
+    height: 20px;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 50%;
+    box-shadow: 30px 0px 0px 0px rgba(255, 255, 255, 0.8);
+    position: relative;
+    margin: 36px 26px;
+  }
+
+  .eye:after {
+    background-color: #59488b;
+    width: 10px;
+    height: 10px;
+    box-shadow: 30px 0px 0px 0px #59488b;
+    border-radius: 50%;
+    left: 9px;
+    top: 8px;
+    position: absolute;
+    content: "";
+    -webkit-animation: eyeball 2s linear infinite alternate;
+    -moz-animation: eyeball 2s linear infinite alternate;
+    animation: eyeball 2s linear infinite alternate;
+  }
+  @-webkit-keyframes eyeball {
+    0% {
+      left: 9px;
+    }
+    100% {
+      left: 1px;
+    }
+  }
+  @-moz-keyframes eyeball {
+    0% {
+      left: 9px;
+    }
+    100% {
+      left: 1px;
+    }
+  }
+  @keyframes eyeball {
+    0% {
+      left: 9px;
+    }
+    100% {
+      left: 1px;
+    }
+  }
 `;
